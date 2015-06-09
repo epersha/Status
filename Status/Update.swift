de1778a67716d24c1471851b99217d630009ac7b
epersha <eric.persha@gmail.com>
2015-06-08 19:13:17 -0700

    Add Models

---
+++ Status/Update.swift
@@ -0,0 +1,17 @@
+//
+//  Update.swift
+//  Status
+//
+//  Created by Eric Persha on 6/8/15.
+//  Copyright (c) 2015 Eric Persha. All rights reserved.
+//
+
+import Foundation
+
+class Update {
+    
+    var user: User?
+    var text: String?
+    var date: NSDate?
+    
+}
---
+++ User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  Status
+//
+//  Created by Eric Persha on 6/8/15.
+//  Copyright (c) 2015 Eric Persha. All rights reserved.
+//
+
+import Foundation
+
+// modeled off of the twitter app
+class User {
+    var name: String?
+    var bio: String?
+    var username: String?
+    var link: String?
+    var city: String?
+    
+}
