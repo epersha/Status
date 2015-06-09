284321516ae4e27227907be9ecf3911973a68adb
epersha <eric.persha@gmail.com>
2015-06-08 20:02:16 -0700

    Show sample data in table view

--- Status/ViewController.swift
+++ Status/ViewController.swift
@@ -12,11 +12,33 @@ class ViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var updates: [Update]?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = self
+        
+      // TODO: Sample data, remove when getting real data
+        
+        updates = [Update]()
+        
+        // user is the instance of the class User
+        var user = User()
+        user.username = "James"
+        user.name = "James Martinez"
+        user.bio = "Him"
+        user.city = "San Francisco"
+        user.link = "http://somewebsite.com"
+        
+        var update = Update()
+        update.date = NSDate()
+        update.text = "Hello, World"
+        update.user = user
+        
+        // adding an update to the update array
+        updates?.append(update)
         
     }
 
@@ -24,14 +46,31 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         // TODO: Return count of update items
+        
+        if let updatesCount = updates?.count {
+            return updatesCount
+        }
         return 0
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         // TODO: Make this cell reusable
         var cell = UITableViewCell()
+        if let updates = updates {
+            var update = updates[indexPath.row]
+            cell.textLabel?.text = update.text
+        }
         return cell
     }
 
 }
+
+
+
+
+
+
+
+
+
 
